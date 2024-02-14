a3f0485d0e7a2fa2405f34ffa52d0980065c06b5
MUHAMMAD UMAR <142122795+Umar6877@users.noreply.github.com>
2024-02-14 11:04:11 +0500

    basket update

--- main.js
+++ main.js
@@ -1,45 +1,63 @@
 let shop = document.getElementById('shop');
 let shopData = [
-	{   number: 1,
+	{     id: "sdsdsd",
 		src: 'images/img-1.jpg',
 		name:'Casual Shirts',
 		description:'Lorem, ipsum dolor sit amet <br> consectetur adipisicing elit.',
 		price:'45',},
-	{   number: 2,
+	{     id: 'dsdfdf',
 		src: 'images/img-2.jpg',
 		name:'Office Shirts',
 		description:'Lorem, ipsum dolor sit amet <br> consectetur adipisicing elit.',
 		price:'100',},
-	{   number: 3,
+	{   id: 'sdsds',
 		src: 'images/img-3.jpg',
 		name:'T Shirts',
 		description:'Lorem, ipsum dolor sit amet <br> consectetur adipisicing elit.',
 		price:'25',},
-	{   number: 4,
+	{   id: 'dssdsdsdf',
 		src: 'images/img-4.jpg',
 		name:'Mens Suit',
 		description:'Lorem, ipsum dolor sit amet <br> consectetur adipisicing elit.',
 		price:'300',},
 	]
 let generateShop = ()=>{
-	return(shop.innerHTML= shopData.map((x)=>{
-		return(shop.innerHTML = `
-		<div class="product">
-		<img src="${x.src}" alt="">
-		<div class="detail">
-			<h3>Casual Shirts</h3>
-			<p>Lorem, ipsum dolor sit amet <br> consectetur adipisicing elit.</p>
-			<div class="price-qty">
-				<h3>$ 45</h3>
-				<div class="qty">
-					<p>+</p>
-					<input type="text" value="0" name="number" id="number" readonly>
-					<p>-</p>
-				</div>
+	return shop.innerHTML= shopData.map((x)=>{
+	let {id,src,name,description,price}= x
+	return(`
+	<div id="product-${id}" class="product">
+	<img src="${src}" alt="">
+	<div class="detail">
+		<h3>${name}</h3>
+		<p>${description}</p>
+		<div class="price-qty">
+			<h3>$ ${price}</h3>
+			<div class="qty">
+			<p onclick="decrement(${id})">-</p>
+			<input type="text" value="0" name="number" id="${id}" readonly>
+			<p onclick="increment(${id})">+</p>
 			</div>
 		</div>
-	</div>`)
-	}))
-
+	</div>
+	</div>
+	`)
+	}).join("");
 };
-generateShop()
+generateShop();
+let basket = []
+console.log(basket);
+let increment = (id)=>{
+	let selectItem = id;
+      basket.push({
+		id:selectItem.id,
+		item:1,
+	})
+	console.log(basket);
+};
+let decrement = (id)=>{
+	let selectItem = id;
+	console.log(selectItem.id)
+};
+let update = ()=>{};
+
+
