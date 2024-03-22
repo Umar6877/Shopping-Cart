1a56eb122c9602bd8a0fa3b90257c778a33d85ee
MUHAMMAD UMAR <142122795+Umar6877@users.noreply.github.com>
2024-03-22 16:26:29 +0500

    seacrh bar was added with stying

--- src/main.js
+++ src/main.js
@@ -1,13 +1,14 @@
-let shop = document.getElementById('shop');
-let input = document.getElementsByClassName('input');
-let cartAmount = document.getElementById('cartAmount');
+let shop = document.getElementById("shop");
+let input = document.getElementsByClassName("input");
+let cartAmount = document.getElementById("cartAmount");
 
-let basket = JSON.parse(localStorage.getItem("data")) || []
-let generateShop = ()=>{
-	return shop.innerHTML= shopData.map((x)=>{
-	let {id,src,name,description,price}= x;
-	let search = basket.find((x)=> x.id === id) || []
-	return`
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+let generateShop = () => {
+  return (shop.innerHTML = shopData
+    .map((x) => {
+      let { id, src, name, description, price } = x;
+      let search = basket.find((x) => x.id === id) || [];
+      return `
 	<div id="main-product" class="main-product">
             <img width="100" height="100" src="${src}" alt="">
             <div class="main-detail">
@@ -25,59 +26,64 @@ let generateShop = ()=>{
                 </div>
             </div>
         </div>
-	`
-	}).join("");
+	`;
+    })
+    .join(""));
 };
 generateShop();
-      
-let increment = (id)=>{
-	let selectItem = id;
-	let search = basket.find((x)=> x.id === selectItem.id);
-	if (search === undefined) {
-		basket.push({
-			id: selectItem.id,
-			item: 1,
-		})
-	}else{
-		search.item += 1;
-	}
-	basket = basket.filter((x)=> x.item !== 0);    // i have to know about "filter"
-	update(selectItem.id)
-	localStorage.setItem("data", JSON.stringify(basket))
+
+let increment = (id) => {
+  let selectItem = id;
+  let search = basket.find((x) => x.id === selectItem.id);
+  if (search === undefined) {
+    basket.push({
+      id: selectItem.id,
+      item: 1,
+    });
+  } else {
+    search.item += 1;
+  }
+  basket = basket.filter((x) => x.item !== 0); // i have to know about "filter"
+  update(selectItem.id);
+  localStorage.setItem("data", JSON.stringify(basket));
 };
-let decrement = (id)=>{
-	let selectItem = id;
-	let search = basket.find((x)=> x.id === selectItem.id);
-	if (search === undefined) return;
-	if (search.item === 0 || undefined) return;
-	else{
-		search.item -= 1
-	}
-	update(selectItem.id)
-	basket = basket.filter((x)=> x.item !== 0);
-    localStorage.setItem("data", JSON.stringify(basket))
+let decrement = (id) => {
+  let selectItem = id;
+  let search = basket.find((x) => x.id === selectItem.id);
+  if (search === undefined) return;
+  if (search.item === 0 || undefined) return;
+  else {
+    search.item -= 1;
+  }
+  update(selectItem.id);
+  basket = basket.filter((x) => x.item !== 0);
+  localStorage.setItem("data", JSON.stringify(basket));
 };
-let update = (id)=>{
-	let search = basket.find((x)=> x.id === id)
-	document.getElementById(id).innerHTML = search.item;
-      calculation();
-      localStorage.setItem("data", JSON.stringify(basket))
-}; 
-let calculation = ()=>{
-      let cartAmount = document.getElementById('cartAmount');
-      cartAmount.innerHTML = (basket.map((x)=> x.item).reduce((x,y)=> x+y,0));
-}
+let update = (id) => {
+  let search = basket.find((x) => x.id === id);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+let calculation = () => {
+  let cartAmount = document.getElementById("cartAmount");
+  cartAmount.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
 calculation();
 if (cartAmount.innerText >= 1000) {
-      cartAmount.innerHTML = "1K+"
+  cartAmount.innerHTML = "1K+";
 }
 
 function CheckFirstVisit() {
-	let isfirstVisit = localStorage.getItem('isfirstVist');
-	
-	if (!isfirstVisit) {
-		localStorage.clear();
-		localStorage.setItem('isfirstVist', true)
-	}
+  let isfirstVisit = localStorage.getItem("isfirstVist");
+
+  if (!isfirstVisit) {
+    localStorage.clear();
+    localStorage.setItem("isfirstVist", true);
+  }
 }
 CheckFirstVisit();
+
+// search  functionality
+
+let searchFun = () => {};
